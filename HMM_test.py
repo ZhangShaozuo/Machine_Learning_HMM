5409dfd1f61bd101718490beb4315b4a84a60fc4
ZhangShaozuo <zsz2628836781@gmail.com>
2020-12-10 23:10:14 +0800

    delete

--- HMM_test.py
+++
@@ -1,8 +0,0 @@
-from HMM_part3 import load_data
-
-if __name__=="__main__":
-    trainpath = 'EN\\train'
-    data, tokenOcc, tagOcc = load_data(trainpath)
-    print(data.head())
-    print(tagOcc.head())
-    print(tagOcc+0.5)
